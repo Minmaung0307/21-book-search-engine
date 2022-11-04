38cb30da0e69029d66b307a30b2666b6cade524a
Min Maung <minmaung0307@gmail.com>
2022-11-04 17:06:13 -0400

    following module-19 initial setup

--- server/server.js
+++ server/server.js
@@ -1,21 +1,14 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('../client/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+require('./routes/htmlRoutes.js')(app);
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
 });
